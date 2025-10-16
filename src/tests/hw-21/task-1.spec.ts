335f3357eeedbe54ad1ef154a7c50e48de993ff8
aharashchuk <a.harashchuk@gmail.com>
2025-10-16 14:18:10 +0200

    task-1

--- src/tests/hw-21/task-1.spec.ts
+++ src/tests/hw-21/task-1.spec.ts
@@ -9,3 +9,101 @@
 // Страница логина:
 //   Username: обязательное
 //   Password: обязательное
+
+import { test, expect } from "@playwright/test";
+
+interface ICredentials {
+  credentials: {
+    username: string;
+    password: string;
+  };
+}
+
+interface IInvalidCredentials extends ICredentials {
+  notification: NOTIFICATIONS;
+  title: string;
+}
+
+enum NOTIFICATIONS {
+  REGISTERED = "Successfully registered! Please, click Back to return on login page",
+  SHORT_PASSWORD = "Password should contain at least 8 characters",
+  SHORT_USERNAME = "Username should contain at least 3 characters",
+  BAN_SPACES = "Prefix and postfix spaces are not allowed is username",
+  PASSWORD_MISS_UPPERCASE = "Password should contain at least one character in upper case",
+  PASSWORD_MISS_LOWERCASE = "Password should contain at least one character in lower case",
+  EMPTY_PASSWORD = "Password is required",
+  EMPTY_USERNAME = "Username is required"
+}
+
+const invalidCredentials: IInvalidCredentials[] = [
+  {
+    title: "Not registered with short username",
+    credentials: { username: "us", password: "Password1" },
+    notification: NOTIFICATIONS.SHORT_USERNAME
+  },
+  {
+    title: "Not registered username with spaces",
+    credentials: { username: " ", password: "Password1" },
+    notification: NOTIFICATIONS.BAN_SPACES
+  },
+  {
+    title: "Not registered empty username",
+    credentials: { username: "", password: "Password1" },
+    notification: NOTIFICATIONS.EMPTY_USERNAME
+  },
+  {
+    title: "Not registered username with leading space",
+    credentials: { username: " user3", password: "Password1" },
+    notification: NOTIFICATIONS.BAN_SPACES
+  },
+  {
+    title: "Not registered username with trailing space",
+    credentials: { username: "user4 ", password: "Password1" },
+    notification: NOTIFICATIONS.BAN_SPACES
+  },
+  {
+    title: "Not registered short password",
+    credentials: { username: "user5", password: "pass" },
+    notification: NOTIFICATIONS.SHORT_PASSWORD
+  },
+  {
+    title: "Not registered password without uppercase",
+    credentials: { username: "user6", password: "password" },
+    notification: NOTIFICATIONS.PASSWORD_MISS_UPPERCASE
+  },
+  {
+    title: "Not registered password without lowercase",
+    credentials: { username: "user7", password: "PASSWORD" },
+    notification: NOTIFICATIONS.PASSWORD_MISS_LOWERCASE
+  },
+  {
+    title: "Not registered empty password",
+    credentials: { username: "user8", password: " " },
+    notification: NOTIFICATIONS.EMPTY_PASSWORD
+  }
+];
+
+test.describe("[HW-21][DDT Invalid credentials]", () => {
+  const url = "https://anatoly-karpovich.github.io/demo-login-form/";
+  for (const { title, credentials, message } of invalidCredentials) {
+    test(title, async ({ page }) => {
+      //Selectors
+      const loginFormTitle = page.locator("#loginForm");
+      const toRegisterButton = page.locator("#registerOnLogin");
+      const registerFormTitle = page.locator("#registerForm");
+      const usernameInput = page.locator("#userNameOnRegister");
+      const passwordInput = page.locator("#passwordOnRegister");
+      const registerButton = page.locator("#register");
+      //Steps
+      await page.goto(url);
+      await expect(loginFormTitle).toHaveText("Login");
+      await toRegisterButton.click();
+      await expect(registerFormTitle).toHaveText("Registration");
+      await usernameInput.fill(credentials.username);
+      await passwordInput.fill(credentials.password);
+      await registerButton.click();
+      const notification = page.locator("#errorMessageOnRegister");
+      await expect(notification).toHaveText(message);
+    });
+  }
+});

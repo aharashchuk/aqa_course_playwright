b2d4cf7865dd9fa7fa919fdee6503d3273ade5d4
aharashchuk <a.harashchuk@gmail.com>
2025-11-10 12:29:40 +0100

    fix getAll

--- src/tests/api/products/getAll.spec.ts
+++ src/tests/api/products/getAll.spec.ts
@@ -1,83 +1,35 @@
-import test, { expect } from "@playwright/test";
-import { apiConfig } from "config/apiConfig";
-import { credentials } from "config/env";
+import { test, expect } from "fixtures/api.fixture";
 import { generateProductData } from "data/salesPortal/products/generateProductData";
 import { createProductSchema } from "data/schemas/products/create.schema";
 import { getAllProductsSchema } from "data/schemas/products/getAll.schema";
 import { STATUS_CODES } from "data/statusCodes";
 import _ from "lodash";
-import { validateResponse } from "utils/validateResponse.utils";
+import { validateResponse } from "utils/validation/validateResponse.utils";
 import { loginSchema } from "data/schemas/auth/login.schema";
 
-const { baseURL, endpoints } = apiConfig;
 
 test.describe("[API] [Sales Portal] [HW-24 Task-2.Get All Products]", () => {
   let id = "";
   let token = "";
 
-  test.afterEach(async ({ request }) => {
-    const response = await request.delete(`${baseURL}${endpoints.productById(id)}`, {
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    expect(response.status()).toBe(STATUS_CODES.DELETED);
+  test.afterEach(async ({ productsApiService }) => {
+    if (id) await productsApiService.delete(token, id);
   });
 
-  test("Get All Products", async ({ request }) => {
-    const loginResponse = await request.post(baseURL + endpoints.login, {
-      data: credentials,
-      headers: {
-        "content-type": "application/json",
-      }
-    });
+  test("Get All Products", async ({ loginApiService, productsApiService, productsApi }) => {
+    // const loginResponse = await request.post(baseURL + endpoints.login, {
+    //   data: credentials,
+    //   headers: {
+    //     "content-type": "application/json",
+    //   }
+    // });
+    const token = await loginApiService.loginAsAdmin();
+    const createdProduct = await productsApiService.create(token);
+    const id = createdProduct._id;
 
-    await validateResponse(loginResponse, {
-      status: STATUS_CODES.OK,
-      schema: loginSchema,
-      IsSuccess: true,
-      ErrorMessage: null
-    });
+    const getAllProductsResponse = await productsApi.getAll(token);
 
-    const loginBody = await loginResponse.json();
-    expect.soft(loginBody.User.username).toBe(credentials.username);
-
-    const headers = loginResponse.headers();
-    token = headers["authorization"]!;
-    expect(token).toBeTruthy();
-
-    const productData = generateProductData();
-    const createProductResponse = await request.post(baseURL + endpoints.products, {
-      data: productData,
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-
-    const createProductBody = await createProductResponse.json();
-    await validateResponse(createProductResponse, {
-      status: STATUS_CODES.CREATED,
-      schema: createProductSchema,
-      IsSuccess: true,
-      ErrorMessage: null,
-    });
-
-    const actualProductData = createProductBody.Product;
-
-    expect(_.omit(actualProductData, ["_id", "createdOn"])).toEqual(productData);
-
-    id = actualProductData._id;
-
-
-    const getAllProductsResponse = await request.get(`${baseURL}${endpoints.productsAll}`, {
-      headers: {
-        "content-type": "application/json",
-        Authorization: `Bearer ${token}`,
-      }
-    });
-    const getAllProductsBody = await getAllProductsResponse.json();
+    const getAllProductsBody = getAllProductsResponse.body;
     await validateResponse(getAllProductsResponse, {
       status: STATUS_CODES.OK,
       schema: getAllProductsSchema,
@@ -85,6 +37,6 @@ test.describe("[API] [Sales Portal] [HW-24 Task-2.Get All Products]", () => {
       ErrorMessage: null
     });
     const product = getAllProductsBody["Products"].find((prod: { _id: string; }) => prod._id === id);
-    expect(_.omit(product, ["_id", "createdOn"])).toEqual(productData);
+    expect(product).toEqual(createdProduct);
   });
 });
